91862edb3b77a428cd60cecf45d454c85723429f
maksimv0202 <maksimv0202@gmail.com>
2025-05-02 21:20:40 +0300

    feat: pytest stub

---
+++ tests/conftest.py
@@ -0,0 +1,46 @@
+from typing import AsyncGenerator
+
+import pytest_asyncio
+from httpx import AsyncClient, ASGITransport
+from sqlalchemy import StaticPool
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from core.db import get_async_session
+from core.models import Base
+from main import app
+
+
+@pytest_asyncio.fixture(scope='function')
+async def async_engine():
+    engine = create_async_engine(
+        'sqlite+aiosqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool
+    )
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield engine
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest_asyncio.fixture
+async def async_session(async_engine) -> AsyncGenerator[AsyncSession, None]:
+    async_session = async_sessionmaker(async_engine, expire_on_commit=False)
+    async with async_session() as session:
+        yield session
+
+
+@pytest_asyncio.fixture
+async def async_client(async_session) -> AsyncGenerator[AsyncClient, None]:
+
+    async def override_get_async_session():
+        yield async_session
+
+    app.dependency_overrides[get_async_session] = override_get_async_session
+
+    async with AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url='http://127.0.0.1:8000/api/v1/'
+    ) as client:
+        yield client
---
+++ tests/unit/test_building.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_add_building(async_client):
+    assert True is True
